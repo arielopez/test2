2c414f14532bd78a6bf1ed4f2b4abe22b96c18f5
Alberto Lopez <albertoariel07@gmail.com>
2016-04-11 06:33:35 -0300

    nuevas tabla 20160411

---
+++ plugins/agenda/model/tarea_agenda.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 10/04/16
+ * Time: 10:31 PM
+ */
+class tarea_agenda extends fs_model
+{
+
+    /**
+     * Esta función es llamada al crear una tabla.
+     * Permite insertar valores en la tabla.
+     */
+    protected function install()
+    {
+        // TODO: Implement install() method.
+    }
+
+    /**
+     * Esta función devuelve TRUE si los datos del objeto se encuentran
+     * en la base de datos.
+     */
+    public function exists()
+    {
+        // TODO: Implement exists() method.
+    }
+
+    /**
+     * Esta función sirve tanto para insertar como para actualizar
+     * los datos del objeto en la base de datos.
+     */
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * Esta función sirve para eliminar los datos del objeto de la base de datos
+     */
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+}
